91a56d526d244ad7fe3e8cc840dcf166edd3c998
Juliet Wilson <juliet695582@gmail.com>
2025-06-23 01:03:45 +0530

    Initial setup and landing page

---
+++ watchlist_app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Landing from './components/LandingPage/Landing';
+import Anime from './components/Anime/Anime';
+
+function App() {
+  return (
+    <div>
+      <Landing />
+      <Anime />
+      {/* You can add more sections below */}
+    </div>
+  );
+}
+
+export default App;
---
+++ watchlist_app/src/components/LandingPage/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Landing.css';
+
+const Landing = () => {
+  return (
+    <div className="landing-container">
+      <h1 className="landing-text">Find your squads suggestions here</h1>
+    </div>
+  );
+};
+
+export default Landing;
